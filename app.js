7dea4871fbc27e68e30e3cc71e3919ed157657a8
Prashant Kumar <prax.pks@gmail.com>
2025-06-25 16:37:53 +0530

    Second commit

--- app.js
+++ app.js
@@ -186,6 +186,7 @@
 
 const _ = require("lodash");
 
-const items = [1, [2, [3, [4]]]];
+const items = [1, [2, [3, [4, [5]]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+console.log("Diabolical");
